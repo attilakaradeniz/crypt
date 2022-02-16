2895147a91f4eeadb9670a002cd7d54c00e1de69
attilakaradeniz <attilakaradeniz@gmail.com>
2022-02-16 07:48:52 +0100

    added initial files & folders

---
+++ js/main.js
@@ -0,0 +1,30 @@
+
+
+$(document).ready(function(){
+
+    let blip = document.getElementsByTagName("audio")[0];
+    let box = $(".boxes");
+    
+    // let box = document.getElementsByClassName("boxes");
+    
+    console.log("testos");
+    
+    
+    
+    
+    box.mouseenter(function() {
+        blip.play();
+        
+    });
+    
+    box.on("mouseover", function() {
+        blip.play();
+        console.log("su an mouseover");
+        
+    });
+
+
+
+});
+
+
